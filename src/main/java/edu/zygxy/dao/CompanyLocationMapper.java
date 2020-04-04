2cae9a059b6a2e311f25658fdccc7a97ad7a832f
Long-Liu <1263601750@qq.com>
2020-04-04 15:13:03 +0800

    fix

--- src/main/java/edu/zygxy/dao/CompanyLocationMapper.java
+++ src/main/java/edu/zygxy/dao/CompanyLocationMapper.java
@@ -2,6 +2,7 @@ package edu.zygxy.dao;
 
 import edu.zygxy.pojo.CompanyLocation;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,4 +11,10 @@ public interface CompanyLocationMapper {
     List<CompanyLocation> selectList();
 
     void deleteById(Integer id);
+
+    void insert(@Param("o") CompanyLocation o);
+
+    void updateById(@Param("id") Integer id, @Param("o") CompanyLocation o);
+
+    CompanyLocation selectById(Integer id);
 }
--- src/main/java/edu/zygxy/web/CompanyLocationController.java
+++ src/main/java/edu/zygxy/web/CompanyLocationController.java
@@ -1,6 +1,7 @@
 package edu.zygxy.web;
 
 import edu.zygxy.dao.CompanyLocationMapper;
+import edu.zygxy.pojo.CompanyLocation;
 import edu.zygxy.pojo.JsonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -12,16 +13,42 @@ import org.springframework.web.bind.annotation.*;
 public class CompanyLocationController {
     private final CompanyLocationMapper companyLocationMapper;
 
-    @GetMapping("/company")
+    @GetMapping("/company_location")
     public String company(ModelMap modelMap) {
         modelMap.addAttribute("companyLocations", companyLocationMapper.selectList());
         return "company_location";
+    }
+
+    @GetMapping("company/location/updatePage")
+    public String updatePage(@RequestParam int id, ModelMap modelMap) {
+        CompanyLocation o = companyLocationMapper.selectById(id);
+        modelMap.addAttribute("cl", o);
+        return "company_location_update";
+    }
+
+    @GetMapping("company/location/insertPage")
+    public String insertPage() {
+        return "company_location_insert";
     }
 
     @ResponseBody
     @DeleteMapping("/api/company/location/{id}")
     public JsonResponse deleteById(@PathVariable Integer id) {
         companyLocationMapper.deleteById(id);
+        return new JsonResponse(null);
+    }
+
+    @ResponseBody
+    @PutMapping("/api/company/location")
+    public JsonResponse deleteById(@RequestBody CompanyLocation o) {
+        companyLocationMapper.insert(o);
+        return new JsonResponse(null);
+    }
+
+    @ResponseBody
+    @PostMapping("/api/company/location/{id}")
+    public JsonResponse updateById(@PathVariable Integer id, @RequestBody CompanyLocation o) {
+        companyLocationMapper.updateById(id, o);
         return new JsonResponse(null);
     }
 }
